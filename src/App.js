2e645846134040281426529464d9cfe785776f08
Kaami Dev <kaami@kaamidev.com>
2020-07-10 19:02:56 -0400

    created and imported header component file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header';
+
 function App() {
 	return (
 		<div>
+			<Header />
 			<p>Hello World!</p>
 		</div>
 	);
