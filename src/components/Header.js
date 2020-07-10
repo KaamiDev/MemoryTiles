bdb55a02405960211aa9845a304f4b86972c2594
Kaami Dev <kaami@kaamidev.com>
2020-07-10 19:12:47 -0400

    created header elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import cubeIcon from '../icons/cube-icon.svg';
+
 const Header = () => {
 	return (
-		<div>
-			<p>Header</p>
+		<div id="header">
+			<img src={cubeIcon} alt="cube icon" />
+			<h1>
+				Memory<span>Tiles</span>
+			</h1>
 		</div>
 	);
 };
