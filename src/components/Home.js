38e6e96dff50be8bc8a792c6a2239a8cbf37047b
Kaami Dev <kaami@kaamidev.com>
2020-07-11 20:38:59 -0400

    created home card elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import arrowIcon from '../icons/arrow-icon.svg';
 
 const Home = () => {
 	return (
-		<div>
-			<p>Home Page</p>
+		<div className="body-content">
+			<div id="home-page">
+				<h3>How good is your memory?</h3>
+				<Link to="/play">
+					Play
+					<img src={arrowIcon} alt="arrow icon" />
+				</Link>
+			</div>
 		</div>
 	);
 };
