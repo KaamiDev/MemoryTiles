b174a750e13c7a1f5d4f1f7bd711adaafdecccf2
Kaami Dev <kaami@kaamidev.com>
2020-07-11 20:13:25 -0400

    setup routes with home and play components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/Header';
+import Home from './components/Home';
+import Play from './components/Play';
 
 function App() {
 	return (
 		<div>
-			<Header />
-			<p>Hello World!</p>
+			<Router>
+				<Header />
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Route path="/play" exact component={Play} />
+				</Switch>
+			</Router>
 		</div>
 	);
 }
---
+++ src/components/Play.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Play = () => {
+	return (
+		<div>
+			<p>Play Page</p>
+		</div>
+	);
+};
+
+export default Play;
