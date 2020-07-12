ed7ecca18d517242c84831f55bfcdda8ee487aac
Kaami Dev <kaami@kaamidev.com>
2020-07-11 20:16:16 -0400

    clicking header redirects to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import cubeIcon from '../icons/cube-icon.svg';
 
 const Header = () => {
 	return (
 		<div id="header">
-			<img src={cubeIcon} alt="cube icon" />
-			<h1>
-				Memory<span>Tiles</span>
-			</h1>
+			<Link to="/">
+				<img src={cubeIcon} alt="cube icon" />
+				<h1>
+					Memory<span>Tiles</span>
+				</h1>
+			</Link>
 		</div>
 	);
 };
