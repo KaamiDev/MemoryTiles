eca286ac2c915a2d1795495564b8b19861a573cc
Kaami Dev <kaami@kaamidev.com>
2020-07-11 21:02:29 -0400

    arrow now moves on hover

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import arrowIcon from '../icons/arrow-icon.svg';
 
 const Home = () => {
+	const [ hovering, setHovering ] = useState(false);
+
 	return (
 		<div className="body-content">
 			<div id="home-page">
 				<h3>How good is your memory?</h3>
-				<Link to="/play">
+				<Link to="/play" onMouseOver={() => setHovering(true)} onMouseLeave={() => setHovering(false)}>
 					Play
-					<img src={arrowIcon} alt="arrow icon" />
+					<img src={arrowIcon} className={hovering ? 'hovering-btn' : ''} alt="arrow icon" />
 				</Link>
 			</div>
 		</div>
