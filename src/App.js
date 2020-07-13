4f8559d047f9d4ce8cf3b0f5a2f5473c3253eedb
Kaami Dev <kaami@kaamidev.com>
2020-07-13 18:10:20 -0400

    added animation css file with some ani styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+import './Animate.css';
 
 import Header from './components/Header';
 import Home from './components/Home';
