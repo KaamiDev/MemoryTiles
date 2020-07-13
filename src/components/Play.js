a1d3c116e8853ee2622aa3d92ce4a119c51f33cb
Kaami Dev <kaami@kaamidev.com>
2020-07-13 18:55:52 -0400

    set starting state to control animations

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Square from './Square';
 
 const Play = () => {
+	const [ starting, setStarting ] = useState(true);
+
 	const squares = Array.from(Array(48).keys()).map((index) => {
 		return <Square number={index} />;
 	});
 
 	return (
 		<div id="play-page">
-			<h3 className="play-caption">Watch carefully for the pattern.</h3>
-			<h6 className="level-small" id={'level-big'}>
+			<h3 className={`play-caption ${starting ? 'play-caption-ani' : ''}`}>Watch carefully for the pattern.</h3>
+			<h6 className="level-small" id={starting ? 'level-big' : ''}>
 				Level - 01
 			</h6>
-			<div className="square-container">{squares}</div>
+			<div className={`square-container ${starting ? 'square-container-ani' : ''}`}>{squares}</div>
 		</div>
 	);
 };
