02ee7099c1ecf87b398aefb971c4675c0932a53e
Kaami Dev <kaami@kaamidev.com>
2020-07-13 19:23:51 -0400

    seperated click to diff function

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,11 +3,15 @@ import React, { useState } from 'react';
 const Square = () => {
 	const [ isGreen, setIsGreen ] = useState(false);
 
-	const handleClick = () => {
+	const turnGreen = () => {
 		setIsGreen(true);
 		setTimeout(() => {
 			setIsGreen(false);
 		}, 1000);
+	};
+
+	const handleClick = () => {
+		turnGreen();
 	};
 
 	return <div onClick={handleClick} className={`square ${isGreen ? 'square-green' : ''}`} />;
