50f4ef061606c16559766d8a2d5586129c2e42dc
Kaami Dev <kaami@kaamidev.com>
2020-07-13 00:50:49 -0400

    created square component

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Square from './Square';
+
 const Play = () => {
 	return (
 		<div id="play-page">
 			<h3>Watch carefully for the pattern.</h3>
 			<h6>Level - 01</h6>
+			<Square />
 		</div>
 	);
 };
---
+++ src/components/Square.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Square = () => {
+	return <div className="square" />;
+};
+
+export default Square;
