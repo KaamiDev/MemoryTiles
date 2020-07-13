33c5081b3a90b8dc0e2e1bd2e4c34cc63ccf55da
Kaami Dev <kaami@kaamidev.com>
2020-07-13 19:12:03 -0400

    create useeffect for game logic

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Square from './Square';
 
 const Play = () => {
+	// const [ level, setLevel ] = useState(1);
 	const [ starting, setStarting ] = useState(true);
 
 	const squares = Array.from(Array(48).keys()).map((index) => {
 		return <Square number={index} />;
 	});
+
+	useEffect(() => {
+		const game = async () => {
+			setTimeout(() => {
+				setStarting(false);
+			}, 2000);
+		};
+		game();
+	}, []);
 
 	return (
 		<div id="play-page">
