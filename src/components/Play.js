f7b1b3a8f81afdcbf54af57172113ccd26ccb60a
Kaami Dev <kaami@kaamidev.com>
2020-07-13 01:26:41 -0400

    squares are now arrays

--- src/components/Play.js
+++ src/components/Play.js
@@ -3,60 +3,15 @@ import React from 'react';
 import Square from './Square';
 
 const Play = () => {
+	const squares = Array.from(Array(48).keys()).map(() => {
+		return <Square />;
+	});
+
 	return (
 		<div id="play-page">
 			<h3>Watch carefully for the pattern.</h3>
 			<h6>Level - 01</h6>
-			<div className="square-container">
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-				<Square />
-			</div>
+			<div className="square-container">{squares}</div>
 		</div>
 	);
 };
