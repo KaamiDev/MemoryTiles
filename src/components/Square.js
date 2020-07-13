855dbd6aa69117bdb894c979eadfa9a971b3bdc7
Kaami Dev <kaami@kaamidev.com>
2020-07-13 19:22:51 -0400

    added click logic to squares

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Square = () => {
-	return <div className="square" />;
+	const [ isGreen, setIsGreen ] = useState(false);
+
+	const handleClick = () => {
+		setIsGreen(true);
+		setTimeout(() => {
+			setIsGreen(false);
+		}, 1000);
+	};
+
+	return <div onClick={handleClick} className={`square ${isGreen ? 'square-green' : ''}`} />;
 };
 
 export default Square;
