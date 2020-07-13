640eaa06ae715e878206b1d52f60790e8f64f7d5
Kaami Dev <kaami@kaamidev.com>
2020-07-13 00:38:18 -0400

    changed caption for play

--- src/components/Play.js
+++ src/components/Play.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Play = () => {
 	return (
 		<div id="play-page">
-			<h3>How good is your memory?</h3>
+			<h3>Watch carefully for the pattern.</h3>
 		</div>
 	);
 };
