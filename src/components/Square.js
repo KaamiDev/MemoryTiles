ed2b5e48e2459778ed0e208dcc0abfd4685e994b
Kaami Dev <kaami@kaamidev.com>
2020-07-14 18:27:04 -0400

    removed audio cuz delay issues

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
 const Square = (props) => {
-	let soundfx = new Audio('/sounds/' + (props.number + 1) + '.mp3');
-	let wrongfx = new Audio('/sounds/wrong.mp3');
 	const [ isGreen, setIsGreen ] = useState(false);
 	const [ isRed, setIsRed ] = useState(false);
 
@@ -19,7 +17,6 @@ const Square = (props) => {
 	);
 
 	const turnGreen = () => {
-		soundfx.play();
 		if (isGreen === true) {
 			setIsGreen(false);
 			setTimeout(() => {
@@ -47,7 +44,6 @@ const Square = (props) => {
 				}, 1000);
 			}
 		} else {
-			wrongfx.play();
 			props.setClickable(false);
 			props.setLosingSquare(props.pattern[0]);
 		}
